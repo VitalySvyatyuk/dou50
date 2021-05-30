2e3eb3b02105779e8c49301f75029a23b6002fae
Vitaly Svyatyuk <goodgame1945@gmail.com>
2021-05-30 23:06:22 +0300

    return analog view

--- main/views.py
+++ main/views.py
@@ -1,2 +1,11 @@
-from django.http import HttpResponseRedirect
 from django.views.generic.base import View
+from django.http import HttpResponseRedirect
+
+
+class AnalogView(View):
+    def get(self, request):
+        if 'analog' in request.session:
+            request.session.pop('analog')
+        else:
+            request.session['analog'] = True
+        return HttpResponseRedirect(request.META['HTTP_REFERER'])
